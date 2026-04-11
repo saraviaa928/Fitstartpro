b4c09e87e1a6e0baa5c3458aa296bd780c0c55b0
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-11 14:44:30 -0600

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
