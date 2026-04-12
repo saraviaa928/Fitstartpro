ee8eb3334435e686d0db6381e22d1635c0cb2bf3
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-12 10:26:51 -0600

    Fix app route

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20, textAlign: "center" }}>
+      <h1>💪 FitStartPro</h1>
+      <p>Tu app de rutinas y nutrición</p>
+    </main>
+  );
+}
