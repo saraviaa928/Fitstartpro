8d81a5f98ad9dda942779946113e73435c502b21
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-11 18:41:37 -0600

    Actualizar layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,11 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "FitStartPro",
-  description: "App de rutinas y nutrición para un estilo de vida saludable",
-};
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}): JSX.Element {
+}) {
   return (
-    <html lang="es">
-      <body style={{ margin: 0, padding: 0 }}>
-        {children}
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   );
 }
