c7d335782c439449635a5f11a24787686a5cf654
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-11 23:02:25 -0600

    Create Rutinas component for workout routines

---
+++ app/rutinas/page.tsx
@@ -0,0 +1,13 @@
+export default function Rutinas() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>🏋️ Rutinas</h1>
+
+      <ul>
+        <li>💪 Principiante (3 días)</li>
+        <li>🔥 Intermedio (4 días)</li>
+        <li>⚡ Avanzado (5 días)</li>
+      </ul>
+    </main>
+  );
+}
