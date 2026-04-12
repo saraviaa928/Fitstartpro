8a5af7b165587c24b82011bae4d6bf083563fa51
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-12 00:37:53 -0600

    Actualizar Page.tsx

--- app/Page.tsx
+++ app/Page.tsx
@@ -1,31 +1,8 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <main style={{ padding: 40, textAlign: "center" }}>
+    <main style={{ padding: "20px", textAlign: "center" }}>
       <h1>💪 FitStartPro</h1>
-      <p>Tu app fitness</p>
-
-      <div style={{ marginTop: 20 }}>
-        <Link href="/rutinas">
-          <button style={styles.button}>Rutinas</button>
-        </Link>
-
-        <Link href="/nutricion">
-          <button style={styles.button}>Nutrición</button>
-        </Link>
-      </div>
+      <p>Tu app de rutinas y nutrición</p>
     </main>
   );
 }
-
-const styles = {
-  button: {
-    padding: "12px 20px",
-    margin: "10px",
-    backgroundColor: "#16a34a",
-    color: "#fff",
-    border: "none",
-    borderRadius: "8px",
-  },
-};
