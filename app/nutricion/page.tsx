314c25aa3851d8be3d897bcde968c1d35af7ca84
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-11 23:03:42 -0600

    Add Nutricion component with meal plan

---
+++ app/nutricion/page.tsx
@@ -0,0 +1,13 @@
+export default function Nutricion() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>🥗 Plan Nutricional</h1>
+
+      <ul>
+        <li>🍗 Proteínas: pollo, huevo</li>
+        <li>🍚 Carbohidratos: arroz, avena</li>
+        <li>🥑 Grasas saludables</li>
+      </ul>
+    </main>
+  );
+}
