699dc212fb183e935131c9d0d578a68588f06b42
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-14 13:43:16 -0600

    Actualizar firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,7 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 // 🔥 CONFIGURACIÓN DE FIREBASE
 const firebaseConfig = {
-  apiKey: "TU_API_KEY_AQUI", // 👈 pega aquí tu apiKey REAL
+  apiKey: "AlzaSy", // 👈 pega aquí tu apiKey REAL
   authDomain: "fitstartpro.firebaseapp.com",
   projectId: "fitstartpro",
   storageBucket: "
