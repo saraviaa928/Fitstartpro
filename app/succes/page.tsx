0ac77039e85e3e9713e30023e5e43eb18904b476
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-15 12:11:00 -0600

    Crear app/succes/page.tsx

---
+++ app/succes/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import { auth, db } from "../../lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
+
+export default function Success() {
+  useEffect(() => {
+    const activarPro = async () => {
+      const user = auth.currentUser;
+      if (!user) return;
+
+      await setDoc(
+        doc(db, "usuarios", user.uid),
+        { pro: true },
+        { merge: true }
+      );
+    };
+
+    activarPro();
+  }, []);
+
+  return <h1>Pago exitoso 🎉 PRO activado</h1>;
+}
