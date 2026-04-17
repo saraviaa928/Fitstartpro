beb019e4a8fcd4581e224cff04ff0076c67d87ee
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-17 16:34:11 -0600

    Actualizar firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: AIzaSyBaOuz28cLkd8GRT6e_3w4S8kYKBnloq6s",
+  apiKey: "AIzaSyBaOuz28cLkd8GRT6e_3w4S8kYKBnloq6s",
   authDomain: "fitstartpro-e8392.firebaseapp.com",
   projectId: "fitstartpro-e8392",
   storageBucket: "fitstartpro-e8392.firebasestorage.app",
-  messagingSenderId: "1073053557711",
-  appId: "1:1073053557711:web:6288c0228f1f72727e68ba",
-  measurementId: "G-ZKF14WM4HV"
+  messagingSenderId: "TU_ID",
+  appId: "TU_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
