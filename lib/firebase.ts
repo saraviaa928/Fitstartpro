986d43a5acf20b1f9eb87361cb8e544583a4a6c8
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-17 13:41:20 -0600

    Actualizar firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,17 +1,19 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+// lib/firebase.ts
+
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpoOjcZKvPJJAhGDiWSheL8HpgXeBUB2o",
-  authDomain: "fitstartpro.firebaseapp.com",
-  projectId: "fitstartpro",
-  storageBucket: "fitstartpro.appspot.com",
-  messagingSenderId: "632203203052",
-  appId: "1:632203203052:web:290c3ced6a43303091f0c9",
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROJECT.firebaseapp.com",
+  projectId: "TU_PROJECT",
+  storageBucket: "TU_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX",
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
