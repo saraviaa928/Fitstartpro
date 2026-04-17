fa6ea98dcfe9199b76645ca0a3ae10e00c2b7c2b
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-17 15:20:01 -0600

    Actualizar firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRiGWCAxMeW8kV0iYb5rxhH-XBOGDkKIc",
+  apiKey: "AIzaSyBaOuz28cLkd8GRT6e_3w4S8kYKBnloq6s",
   authDomain: "TU_PROJECT.firebaseapp.com",
   projectId: "TU_PROJECT_ID",
   storageBucket: "TU_PROJECT.appspot.com",
