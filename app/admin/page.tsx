320f39bef140a9f850407b4f704215426654ca67
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-17 23:55:56 -0600

    Actualizar page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { auth, db } from "../../lib/firebase";
+import { auth, db } from "../@/firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import {
   collection,
