da9f311050a5fbfade42f1197525645c875452f8
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-17 23:48:16 -0600

    Actualizar route.ts

--- app/api/paypal/capture-order/route.ts
+++ app/api/paypal/capture-order/route.ts
@@ -3,8 +3,6 @@ import { captureOrder } from "@/lib/paypal";
 
 export async function POST(req: Request) {
   const { orderId } = await req.json();
-
   const result = await captureOrder(orderId);
-
   return NextResponse.json(result);
 }
