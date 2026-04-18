9e696bf7762c3897e3a275bbdea625b1d7825c4d
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-17 22:55:37 -0600

    Crear capture-order/route.ts

---
+++ app/api/paypal/capture-order/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+import { captureOrder } from "@/lib/paypal";
+
+export async function POST(req: Request) {
+  const { orderId } = await req.json();
+
+  const result = await captureOrder(orderId);
+
+  return NextResponse.json(result);
+}
