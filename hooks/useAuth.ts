c6fe8e37ba35b45dcba1066737ad30875f68e14a
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-18 00:14:34 -0600

    Actualizar useAuth.ts

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { auth } from "../lib/firebase";
+import { auth } from "@/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 
 export default function useAuth() {
