8c9abe712a9c82c3db83fd23965a5d1534d6e24b
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-18 18:16:34 -0600

    Actualizar route.ts

--- app/api/paypal/capture-order/route.ts
+++ app/api/paypal/capture-order/route.ts
@@ -1,8 +1,22 @@
 import { NextResponse } from "next/server";
 import { captureOrder } from "@/lib/paypal";
+import { db } from "@/firebase";
+import { doc, updateDoc, serverTimestamp } from "firebase/firestore";
 
 export async function POST(req: Request) {
-  const { orderId } = await req.json();
+  const { orderId, uid } = await req.json();
+
   const result = await captureOrder(orderId);
+
+  if (result.status === "COMPLETED") {
+    const ref = doc(db, "usuarios", uid);
+
+    await updateDoc(ref, {
+      premium: true,
+      plan: "pro",
+      premiumSince: serverTimestamp(),
+    });
+  }
+
   return NextResponse.json(result);
 }
