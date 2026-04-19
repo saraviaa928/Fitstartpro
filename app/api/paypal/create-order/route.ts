9521f763898e81e44ced937eb102c221cca3b613
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-19 13:10:28 -0600

    Eliminar route.ts

--- app/api/paypal/create-order/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { createOrder } from "@/lib/paypal";
-
-export async function POST(req: Request) {
-  const { value } = await req.json();
-  const order = await createOrder(value);
-  return NextResponse.json({ id: order.id });
-}
