e74f79a13337aaa6984c9aa206a3f6f4dce88b93
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-19 17:30:35 -0600

    Actualizar paypal.ts

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,14 +1,16 @@
+const PAYPAL_API = "https://api-m.sandbox.paypal.com";
 
-
-const CLIENT = process.env.PAYPAL_CLIENT_ID!;
+const CLIENT_ID = process.env.PAYPAL_CLIENT_ID!;
 const SECRET = process.env.PAYPAL_SECRET!;
 
 async function getAccessToken() {
-  const res = await fetch("https://api-m.sandbox.paypal.com/v1/oauth2/token", {
+  const res = await fetch(`${PAYPAL_API}/v1/oauth2/token`, {
     method: "POST",
     headers: {
       Authorization:
-        "Basic " + Buffer.from(CLIENT + ":" + SECRET).toString("base64"),
+        "Basic " +
+        Buffer.from(`${CLIENT_ID}:${SECRET}`).toString("base64"),
+      "Content-Type": "application/x-www-form-urlencoded",
     },
     body: "grant_type=client_credentials",
   });
@@ -17,27 +19,27 @@ async function getAccessToken() {
   return data.access_token;
 }
 
-export async function createSubscription(uid: string) {
+export async function createSubscription() {
   const token = await getAccessToken();
 
-  const res = await fetch(
-    "https://api-m.sandbox.paypal.com/v1/billing/subscriptions",
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
+  const res = await fetch(`${PAYPAL_API}/v1/billing/subscriptions`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      plan_id: process.env.PAYPAL_PLAN_ID,
+      application_context: {
+        return_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
+        cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
       },
-      body: JSON.stringify({
-        plan_id: process.env.PAYPAL_PLAN_ID,
-        custom_id: uid,
-        application_context: {
-          return_url: "https://TU-DOMINIO.vercel.app/success",
-          cancel_url: "https://TU-DOMINIO.vercel.app/",
-        },
-      }),
-    }
-  );
+    }),
+  });
 
-  return res.json();
+  const data = await res.json();
+
+  const approve = data.links.find((l: any) => l.rel === "approve");
+
+  return approve?.href;
 }
