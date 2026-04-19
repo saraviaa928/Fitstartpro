110ecaab6050e6f9d8c44a2b16f90b39aa6f1f7a
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-19 13:24:14 -0600

    Actualizar page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { db } from "@/firebase";
+import { db } from "@/lib/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 export default function Admin() {
