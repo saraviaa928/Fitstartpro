c8c88fcdf8850d8a10748d93ae7f627f1fd0989f
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-18 18:17:28 -0600

    Actualizar authService.ts

--- services/authService.ts
+++ services/authService.ts
@@ -1,13 +1,11 @@
+import { auth } from "@/firebase";
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
 } from "firebase/auth";
-import { auth } from "@/firebase";
 
-export const loginUser = (email: string, password: string) => {
-  return signInWithEmailAndPassword(auth, email, password);
-};
+export const loginUser = (email: string, password: string) =>
+  signInWithEmailAndPassword(auth, email, password);
 
-export const registerUser = (email: string, password: string) => {
-  return createUserWithEmailAndPassword(auth, email, password);
-};
+export const registerUser = (email: string, password: string) =>
+  createUserWithEmailAndPassword(auth, email, password);
