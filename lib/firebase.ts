d8c489ac944b8f1084a2ca29a06fad7e1c220d35
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-19 14:50:31 -0600

    Actualizar firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   authDomain: "TU_AUTH_DOMAIN",
   projectId: "TU_PROJECT_ID",
   storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_ID",
+  messagingSenderId: "TU_MSG_ID",
   appId: "TU_APP_ID",
 };
 
