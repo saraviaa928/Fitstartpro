1e0bb59954edbc360a6d3373ea8b87f0aa0c7c11
saraviaa928 <angelsaravia.1620@gmail.com>
2026-04-22 13:58:14 -0600

    Actualizar route.ts

--- app/api/paypal/create-subscription/route.ts
+++ app/api/paypal/create-subscription/route.ts
@@ -3,13 +3,11 @@ import { createSubscription } from "@/lib/paypal";
 
 export async function POST() {
   try {
-    const data = await createSubscription(
-      process.env.NEXT_PUBLIC_PAYPAL_PLAN_ID!
-    );
+    const data = await createSubscription();
 
     return NextResponse.json(data);
   } catch (error) {
-    console.error(error);
+    console.error("ERROR API:", error);
     return NextResponse.json({ error: "Error creando suscripción" });
   }
 }
